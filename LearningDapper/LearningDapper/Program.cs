d235f5c90841b370a41700326658ce40f59397ef
Edson Maia da Costa Moura <edson.maia@fadami.com.br>
2023-09-23 15:14:46 -0300

    feat: First query.

---
+++ LearningDapper/LearningDapper/Program.cs
@@ -0,0 +1,12 @@
+﻿using Dapper;
+using LearningDapper.Models;
+using Microsoft.Data.SqlClient;
+
+const string connectionString = @"Server=FDM-038\SQLEXPRESS;Database=balta;Integrated Security=SSPI;Trusted_Connection=True;Encrypt=False";
+
+using var connection = new SqlConnection(connectionString);
+var categorys = connection.Query<Category>("SELECT [Id], [Title] FROM Category");
+foreach (var category in categorys)
+{
+    Console.WriteLine($"{category.Id} - {category.Title}");
+}
