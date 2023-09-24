cc487dcdc31f486828fe3f68c2ed79f0c2e0b190
Edson Maia da Costa Moura <edson.maia@fadami.com.br>
2023-09-23 21:54:42 -0300

    feat: update

--- LearningDapper/LearningDapper/Program.cs
+++ LearningDapper/LearningDapper/Program.cs
@@ -4,18 +4,36 @@ using Microsoft.Data.SqlClient;
 
 const string connectionString = @"Server=FDM-038\SQLEXPRESS;Database=balta;Integrated Security=SSPI;Trusted_Connection=True;Encrypt=False";
 
-Category category = new()
-{
-    Id = Guid.NewGuid(),
-    Title = "Amazon AWS",
-    Url = "Amazon",
-    Description = "Categoria destinada a serviços AWS",
-    Order = 8,
-    Summary = "AWS Cloud",
-    Featured = false
-};
 
-var insertSql = @"INSERT INTO 
+using var connection = new SqlConnection(connectionString);
+UpdateCategory(connection);
+//CreateCategory(connection);
+ListCategories(connection);
+
+
+static void ListCategories(SqlConnection connection)
+{
+    var categorys = connection.Query<Category>("SELECT [Id], [Title] FROM Category");
+    foreach (var item in categorys)
+    {
+        Console.WriteLine($"{item.Id} - {item.Title}");
+    }
+}
+
+static void CreateCategory(SqlConnection connection)
+{
+    Category category = new()
+    {
+        Id = Guid.NewGuid(),
+        Title = "Amazon AWS",
+        Url = "Amazon",
+        Description = "Categoria destinada a serviços AWS",
+        Order = 8,
+        Summary = "AWS Cloud",
+        Featured = false
+    };
+
+    var insertSql = @"INSERT INTO 
                         [Category] 
                    VALUES(
                             @Id
@@ -26,23 +44,27 @@ var insertSql = @"INSERT INTO
                         ,   @Description
                         ,   @Featured)";
 
-using var connection = new SqlConnection(connectionString);
+    var rows = connection.Execute(insertSql, new
+    {
+        category.Id,
+        category.Title,
+        category.Url,
+        category.Summary,
+        category.Order,
+        category.Description,
+        category.Featured
+    });
+    Console.WriteLine(rows + " Linhas Inseridas");
+}
 
-var rows = connection.Execute(insertSql, new
+static void UpdateCategory(SqlConnection connection)
 {
-    category.Id,
-    category.Title,
-    category.Url,
-    category.Summary,
-    category.Order,
-    category.Description,
-    category.Featured
-});
+    var updateCategory = "UPDATE [Category] SET [Title] = @Title WHERE [Id] = @Id";
 
-Console.WriteLine(rows + " Linhas Inseridas");
-
-var categorys = connection.Query<Category>("SELECT [Id], [Title] FROM Category");
-foreach (var item in categorys)
-{
-    Console.WriteLine($"{item.Id} - {item.Title}");
+    var rows = connection.Execute(updateCategory, new
+    {
+        Id = new Guid("AF3407AA-11AE-4621-A2EF-2028B85507C4"),
+        Title = "Frontend 2023"
+    });
+    Console.WriteLine(rows + " Linhas Atualizadas");
 }
