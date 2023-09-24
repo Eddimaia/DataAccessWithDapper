b2bace4b24477affc3ad2f712d10714e05d3c767
Edson Maia da Costa Moura <edson.maia@fadami.com.br>
2023-09-23 21:38:12 -0300

    feat: first insert

--- LearningDapper/LearningDapper/Program.cs
+++ LearningDapper/LearningDapper/Program.cs
@@ -4,9 +4,45 @@ using Microsoft.Data.SqlClient;
 
 const string connectionString = @"Server=FDM-038\SQLEXPRESS;Database=balta;Integrated Security=SSPI;Trusted_Connection=True;Encrypt=False";
 
-using var connection = new SqlConnection(connectionString);
-var categorys = connection.Query<Category>("SELECT [Id], [Title] FROM Category");
-foreach (var category in categorys)
+Category category = new()
 {
-    Console.WriteLine($"{category.Id} - {category.Title}");
+    Id = Guid.NewGuid(),
+    Title = "Amazon AWS",
+    Url = "Amazon",
+    Description = "Categoria destinada a serviços AWS",
+    Order = 8,
+    Summary = "AWS Cloud",
+    Featured = false
+};
+
+var insertSql = @"INSERT INTO 
+                        [Category] 
+                   VALUES(
+                            @Id
+                        ,   @Title
+                        ,   @Url
+                        ,   @Summary
+                        ,   @Order
+                        ,   @Description
+                        ,   @Featured)";
+
+using var connection = new SqlConnection(connectionString);
+
+var rows = connection.Execute(insertSql, new
+{
+    category.Id,
+    category.Title,
+    category.Url,
+    category.Summary,
+    category.Order,
+    category.Description,
+    category.Featured
+});
+
+Console.WriteLine(rows + " Linhas Inseridas");
+
+var categorys = connection.Query<Category>("SELECT [Id], [Title] FROM Category");
+foreach (var item in categorys)
+{
+    Console.WriteLine($"{item.Id} - {item.Title}");
 }
