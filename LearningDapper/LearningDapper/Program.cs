e5152100df96cb58b20b3f34d82ada41e89b82c3
Edson Maia da Costa Moura <edson.maia@fadami.com.br>
2023-09-23 22:10:41 -0300

    refactor: cleaning

--- LearningDapper/LearningDapper/Program.cs
+++ LearningDapper/LearningDapper/Program.cs
@@ -1,83 +1,13 @@
-﻿using Dapper;
-using LearningDapper.Models;
+﻿using LearningDapper.Repositories;
 using Microsoft.Data.SqlClient;
 
 const string connectionString = @"Server=FDM-038\SQLEXPRESS;Database=balta;Integrated Security=SSPI;Trusted_Connection=True;Encrypt=False";
 
-
 using var connection = new SqlConnection(connectionString);
-UpdateCategory(connection);
-var id = CreateCategory(connection);
-DeleteCategory(connection, id);
-ListCategories(connection);
+connection.UpdateCategory();
+var id = connection.CreateCategory();
+connection.DeleteCategory(id);
+connection.ListCategories();
 
 
-static void ListCategories(SqlConnection connection)
-{
-    var categorys = connection.Query<Category>("SELECT [Id], [Title] FROM Category");
-    foreach (var item in categorys)
-    {
-        Console.WriteLine($"{item.Id} - {item.Title}");
-    }
-}
 
-static Guid CreateCategory(SqlConnection connection)
-{
-    Category category = new()
-    {
-        Id = Guid.NewGuid(),
-        Title = "Amazon AWS",
-        Url = "Amazon",
-        Description = "Categoria destinada a serviços AWS",
-        Order = 8,
-        Summary = "AWS Cloud",
-        Featured = false
-    };
-
-    var insertSql = @"INSERT INTO 
-                        [Category] 
-                   VALUES(
-                            @Id
-                        ,   @Title
-                        ,   @Url
-                        ,   @Summary
-                        ,   @Order
-                        ,   @Description
-                        ,   @Featured)";
-
-    var rows = connection.Execute(insertSql, new
-    {
-        category.Id,
-        category.Title,
-        category.Url,
-        category.Summary,
-        category.Order,
-        category.Description,
-        category.Featured
-    });
-    Console.WriteLine(rows + " Linhas Inseridas");
-
-    ListCategories(connection);
-
-    return category.Id;
-}
-
-static void UpdateCategory(SqlConnection connection)
-{
-    var updateCategory = "UPDATE [Category] SET [Title] = @Title WHERE [Id] = @Id";
-
-    var rows = connection.Execute(updateCategory, new
-    {
-        Id = new Guid("AF3407AA-11AE-4621-A2EF-2028B85507C4"),
-        Title = "Frontend 2023"
-    });
-    Console.WriteLine(rows + " Linhas Atualizadas");
-}
-
-static void DeleteCategory(SqlConnection connection, Guid id)
-{
-    var updateCategory = "DELETE [Category] WHERE [Id] = @Id";
-
-    var rows = connection.Execute(updateCategory, new { Id = id });
-    Console.WriteLine(rows + " Linhas Deletadas");
-}
---
+++ LearningDapper/LearningDapper/Repositories/CatergoryRepository.cs
@@ -0,0 +1,79 @@
+﻿using Dapper;
+using LearningDapper.Models;
+using Microsoft.Data.SqlClient;
+
+namespace LearningDapper.Repositories
+{
+    public static class CatergoryRepository
+    {
+        public static void ListCategories(this SqlConnection connection)
+        {
+            var categorys = connection.Query<Category>("SELECT [Id], [Title] FROM Category");
+            foreach (var item in categorys)
+            {
+                Console.WriteLine($"{item.Id} - {item.Title}");
+            }
+        }
+
+        public static Guid CreateCategory(this SqlConnection connection)
+        {
+            Category category = new()
+            {
+                Id = Guid.NewGuid(),
+                Title = "Amazon AWS",
+                Url = "Amazon",
+                Description = "Categoria destinada a serviços AWS",
+                Order = 8,
+                Summary = "AWS Cloud",
+                Featured = false
+            };
+
+            var insertSql = @"INSERT INTO 
+                        [Category] 
+                   VALUES(
+                            @Id
+                        ,   @Title
+                        ,   @Url
+                        ,   @Summary
+                        ,   @Order
+                        ,   @Description
+                        ,   @Featured)";
+
+            var rows = connection.Execute(insertSql, new
+            {
+                category.Id,
+                category.Title,
+                category.Url,
+                category.Summary,
+                category.Order,
+                category.Description,
+                category.Featured
+            });
+            Console.WriteLine(rows + " Linhas Inseridas");
+
+            ListCategories(connection);
+
+            return category.Id;
+        }
+
+        public static void UpdateCategory(this SqlConnection connection)
+        {
+            var updateCategory = "UPDATE [Category] SET [Title] = @Title WHERE [Id] = @Id";
+
+            var rows = connection.Execute(updateCategory, new
+            {
+                Id = new Guid("AF3407AA-11AE-4621-A2EF-2028B85507C4"),
+                Title = "Frontend 2023"
+            });
+            Console.WriteLine(rows + " Linhas Atualizadas");
+        }
+
+        public static void DeleteCategory(this SqlConnection connection, Guid id)
+        {
+            var updateCategory = "DELETE [Category] WHERE [Id] = @Id";
+
+            var rows = connection.Execute(updateCategory, new { Id = id });
+            Console.WriteLine(rows + " Linhas Deletadas");
+        }
+    }
+}
